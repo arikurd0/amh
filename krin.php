5b1186bb850da580a78c9d624de6947cef250ba0
arikurd0 <142266282+arikurd0@users.noreply.github.com>
2023-12-18 22:29:05 +0300

    Add files via upload

---
+++ krin.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <title>WELCOME</title>
+</head>
+<body>
+  
+<center>
+<div class="d-flex flex-column align-items-center">
+    <h3 class="mb-3">ژ بو كرينا كورسى پەيوەنديێ ب مە بكە</h3>
+    <ul>
+        <li><span class="social-icon social-instagram"><i class="fa fa-whatsapp"></i></span></li>
+        <li><span class="social-icon social-twitter"><i class="fa fa-instagram"></i></span></li>
+        <li><span class="social-icon social-twitter"><i class="fa fa-telegram"></i></span></li>
+
+    </ul>
+</div>
+</center>
+
+<style>
+    @import url("https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800&display=swap");
+
+body {
+    background-color: skyblue;
+    font-family: "Poppins", sans-serif;
+    font-weight: 500px;
+    height: 100vh;
+    display: flex;
+    align-items: center;
+    justify-content: center
+    
+}
+.mb-3{
+    width: 80% !important;
+    text-align: center;
+    padding: 10px;
+    color: whitesmoke;
+    display: flex;
+    background-color: #2C2928 ;
+    border-radius: 9px;
+    margin-left: 40px;
+}
+
+ul {
+    list-style: none;
+    display: flex
+    
+}
+
+.social-icon {
+    height: 90px;
+    width: 100px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    color: #fff;
+    font-size: 27px;
+    margin-right: 11px;
+    cursor: pointer
+}
+
+.social-twitter {
+    background-color: #55acee
+    
+}
+
+.social-facebook {
+    background-color: #3b5999
+}
+
+.social-linkedin {
+    background-color: #0077B5
+}
+
+.social-google {
+    background-color: #dd4b39
+}
+
+.social-instagram {
+    background-color: #dd4b39
+}
+
+.social-icon i {
+    transition: 0.4s all
+}
+
+.social-icon:hover i {
+    transform: scale(1.4)
+}
+</style>
+
+
+</html>
+
+    
+</body>
